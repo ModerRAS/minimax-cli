970886526720f39f059d15bc6b3ef924267a27b3
ModerRAS <adqewsx@gmail.com>
2026-03-27 11:51:18 +0800

    fix: correct clippy allow syntax (dead_code, not clippy::dead_code)

--- src/keyring.rs
+++ src/keyring.rs
@@ -1,5 +1,3 @@
-#![allow(clippy::dead_code)]
-
 //! Secure storage of API key using system keyring/credential store.
 //!
 //! Supports: Linux (Secret Service), macOS (Keychain), Windows (Credential Manager)
